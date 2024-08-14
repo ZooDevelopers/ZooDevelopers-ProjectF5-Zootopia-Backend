d6c7f1bc97734037aa1d6df1594d1f588f2fe1d6
NachoR9 <nachorm9@hotmail.com>
2024-08-14 11:51:22 +0200

    dto done

---
+++ src/main/java/org/factoriaf5/zootopia/dtos/UserDto.java
@@ -0,0 +1,14 @@
+package org.factoriaf5.zootopia.dtos;
+
+public class UserDto {
+    private String username;
+    private String password;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+}
